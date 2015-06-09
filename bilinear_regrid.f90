d4dd535a6b154f607aaf45f6ed6546719225c6a8
azumkehr <azumkehr@ucmerced.edu>
2015-06-09 14:30:13 -0700

    bilinear code now can read the input nc file, get 1 data variable and get the lat lon grid of that data.

--- bilinear_regrid.f90
+++ bilinear_regrid.f90
@@ -1,15 +1,42 @@
       program bilinear_interpolation
         implicit none
-        real testval
-        testval = 0.
-        print*, 'testval',testval
-        call bilinear_interp(2.,3.,2.,3.,2.,3.,2.,3.,5.,6.,8.,9.,2.5,&
-             2.5,testval)
-        print*,'testval', testval
+        INTEGER, PARAMETER::&
+         NROWS= 181,&
+         NCOLS= 288,&
+         NSTEPS= 744,&
+         NLANDPOINTS=12246,&
+         OUT_NROWS = 309,&
+         OUT_NCOLS = 201 ,&
+         OUT_NSTEPS = NSTEPS 
+        real, dimension(nrows,ncols,nsteps)::VAR_DATA
+        real, dimension(OUT_NROWS,OUT_NCOLS,OUT_NSTEPS)::OUT_GRID
+        real, dimension(nrows,ncols)::inlats, inlons !2d arrays of lats and lons
+        integer::i,j  
+
+        character(len=255)::FILENAME
+        character(len=255)::VARNAME
+
+
+
+        FILENAME="/home/ecampbell_lab/SIB/flux_hourly_201005p001.nc"
+        VARNAME="OCS_flux"
+        !call bilinear_interp(2.,3.,2.,3.,2.,3.,2.,3.,5.,6.,8.,9.,2.5,&
+        !     2.5,testval)
+       call get_sib_variable(FILENAME,VARNAME,VAR_DATA,inlats,inlons)  
+       print*,"SHAPE: ",shape(VAR_DATA)
+       print*,"SUM: ", sum(VAR_DATA)
+       print*,"MIN: ", minval(VAR_DATA)
+       print*,"MAX: ", maxval(VAR_DATA)
+       do i = 1, nrows
+        do j = 1, ncols
+         print*, inlats(i,j), inlons(i,j)
+        enddo
+        print*,"___________________________________________"
+       enddo
       end program bilinear_interpolation
 
       
-
+      ! computes linear interpolation
       subroutine linear_interp(x0,x1,y0,y1,x,y)
        implicit none
        real::&
@@ -26,7 +53,7 @@
 
       end subroutine linear_interp
      
-
+      ! Computes a bilinear interplotion
       subroutine bilinear_interp(x0,x1,x2,x3,y0,y1,y2,y3,z0,z1,z2,z3,&
           x,y,z)
        implicit none
@@ -67,6 +94,120 @@
        z = ztemp
        return
 
-
-
       end subroutine bilinear_interp
+      
+      ! Gets the data of VARNAME from a net cdf file. Requires that VAR_DATA
+      ! Is of the correct dimensions. must know dimensions 
+      subroutine get_sib_variable(FILENAME,VARNAME,VAR_DATA,inlats,&
+                 inlons)
+       use netcdf
+       implicit NONE
+       include 'netcdf.inc'
+       include 'PARMS3.EXT'
+       include 'FDESC3.EXT'
+       include 'IODECL3.EXT'
+
+       INTEGER, PARAMETER::&
+         NROWS= 181,&
+         NCOLS= 288,&
+         NSTEPS= 744,&
+         NLANDPOINTS=12246 
+       REAL, PARAMETER::&
+         XCELL = 1.25,&
+         YCELL = 1.
+
+       character (len=255)::FILENAME! NC PATH/NAME
+       character (len=255)::VARNAME! Desired Variable Name
+       character (len=*),parameter::&
+         latvar="latindex",&
+         lat_name="latitude",&
+         lonvar="lonindex",&
+         lon_name="longitude"
+       integer::i,j,t! indexes for iterating
+       integer::ncid,varid,latid,lonid,lonid2,latid2!id for nc reading
+       real, dimension(nrows,ncols,nsteps)::VAR_DATA !data array
+       real, dimension(NLANDPOINTS,NSTEPS)::buffer
+       integer, dimension(nlandpoints)::latindex
+       real,dimension(181)::latitude
+       integer, dimension(nlandpoints)::lonindex
+       real,dimension(288)::longitude
+       real,dimension(nrows,ncols)::inlats,inlons
+       real::templat,templon
+       
+       print*,"Open: ",FILENAME
+       call check(nf90_open(FILENAME,NF90_NOWRITE,ncid))
+
+       ! Read in index data
+       print*,"Find variable: ",latvar
+       call check(nf90_inq_varid(ncid,latvar,latid))
+       print*,"Load variable: ",latvar
+       call check(nf90_get_var(ncid,latid,latindex))
+       print*,"Find variable: ",lonvar
+       call check(nf90_inq_varid(ncid,lonvar,lonid))
+       print*,"Load variable: ",lonvar
+       call check(nf90_get_var(ncid,lonid,lonindex))
+
+       !read latitude/longitude data
+       print*,"Find variable: ",lat_name
+       call check(nf90_inq_varid(ncid,lat_name,latid2))
+       print*,"Load variable: ",lat_name
+       call check(nf90_get_var(ncid,latid2,latitude))
+       print*,"Find variable: ",lon_name
+       call check(nf90_inq_varid(ncid,lon_name,lonid2))
+       print*,"Load variable: ",lon_name
+       call check(nf90_get_var(ncid,lonid2,longitude))
+       
+       ! Read actual data
+       print*,"Find variable: ",VARNAME
+       call check(nf90_inq_varid(ncid,VARNAME,varid))
+       print*,"Load variable: ",VARNAME
+       call check(nf90_get_var(ncid,varid,buffer))
+
+       call check(nf90_close(ncid))
+       print*,"File closed."
+ 
+       ! load longitude and latitude arrays
+       templat = -90.
+       templon = -180.
+       do i = nrows, 1,-1
+        do j = 1, ncols
+         inlats(i,j) = templat
+         inlons(i,j) = templon
+         templon=templon+xcell
+        enddo
+        templat=templat+ycell
+        templon = -180.
+       enddo
+
+       !Zero the input array:
+       do i=1,ncols
+        do j=1,nrows
+         do t = 1,nsteps
+          VAR_DATA(i,j,t) = 0.0
+         enddo
+        enddo
+       enddo
+
+       !reshape the data from the buffer into VAR_DATA
+       do i=1,nlandpoints
+        do t=1,nsteps
+         !landsum = landsum+data_in_OCS_flux(i,t)
+         VAR_DATA(lonindex(i),latindex(i),t) = BUFFER(i,t)
+        enddo
+       enddo
+       
+
+       
+       
+       return
+      end subroutine get_sib_variable   
+
+      subroutine check(status)
+       use netcdf
+       integer, intent ( in) :: status
+
+       if(status /= nf90_noerr) then
+        print *, trim(nf90_strerror(status))
+        stop "Stopped"
+       end if
+      end subroutine check
