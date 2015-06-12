e70af87989db5d7dcaefdae39a41422e2accf2ac
azumkehr <azumkehr@ucmerced.edu>
2015-06-12 11:25:11 -0700

    About to redo the whole read from sib

--- bilinear_regrid.f90
+++ bilinear_regrid.f90
@@ -1,5 +1,10 @@
       program bilinear_interpolation
         implicit none
+        include 'netcdf.inc'
+        include 'PARMS3.EXT'
+        include 'FDESC3.EXT'
+        include 'IODECL3.EXT'
+
         INTEGER, PARAMETER::&
          NROWS= 181,&
          NCOLS= 288,&
@@ -10,40 +15,61 @@
          OUT_NSTEPS = NSTEPS 
 !        real, dimension(ncols,nrows,nsteps)::in_cos_flux
         real, dimension(ncols,nrows,nsteps)::in_cos_flux
-        real, dimension(OUT_NCOLS,OUT_NROWS,1)::OCS_FLUX
+        real, dimension(OUT_NCOLS,OUT_NROWS,nsteps)::OCS_FLUX
         real, dimension(ncols,nrows)::inlats, inlons !2d arrays of lats and lons
         real, dimension(out_ncols,out_nrows)::latgrid, longrid !2d arrays of lats and lons
-        integer::i,j,t,i1,i2,i3,i4,j1,j2,j3,j4
+        real,dimension(out_ncols,out_nrows)::write_buffer
+        integer::i,j,t,i1,i2,i3,i4,j1,j2,j3,j4,LOGDEV,day,hour,jdate,jtime
         real::x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4
         real::d,lat,lon,z! calculated distance between 2 coordinates.
         character(len=255)::FILENAME
         character(len=255)::VARNAME
-
+        character(len=255)::outname
+        
 
 
         FILENAME="/home/ecampbell_lab/SIB/flux_hourly_201005p001.nc"
-        VARNAME="OCS_flux"
+        VARNAME="OCS_gpp"
 !        call bilinear_interp(2.,3.,2.,3.,2.,3.,2.,3.,5.,6.,8.,9.,2.5,&
 !             2.5,z)
 !        print*,z
-!        PRINT*,"STEP 1"
+        PRINT*,"STEP 1"
         call get_sib_variable(FILENAME,VARNAME,in_cos_flux,inlats,inlons)  
+        print*,"in_cos_flux",sum(in_cos_flux),shape(in_cos_flux)
+        print*,"lats",minval(inlats),maxval(inlats)
+        print*,"lons",minval(inlons),maxval(inlons)
+        do i=1,ncols
+         do j = 1,nrows
+          if((inlats(i,j).ne.-9.9999998*10.**10.).and.(inlons(i,j).ne.-9.9999998*10.**10.))then
+           print*,inlats(i,j),inlons(i,j),i,j
+           read(*,*)
+          endif
+         enddo
+        enddo
 !        call make_out_grids(OCS_FLUX,latgrid,longrid,NSTEPS)
-!        PRINT*,"STEP 2"
-        call make_out_grids(OCS_FLUX,latgrid,longrid,1)
+        PRINT*,"STEP 2"
+        call make_out_grids(OCS_FLUX,latgrid,longrid,nsteps)
+        call find_nearest4(43.5,-120.5,inlats,inlons,nrows,ncols,i1,i2,i3,i4,j1,j2,j3,j4)
+        print*,i1,j1,i3,i4,j1,j2,j3,j4
+        print*,""
+        print*,"input: ",-120.5,43.5
+        print*,"closest: ",inlons(i1,j1),inlats(i1,j1)
+        print*,"closest: ",inlons(i2,j2),inlats(i2,j2)
+        print*,"closest: ",inlons(i3,j3),inlats(i3,j3)
+        print*,"closest: ",inlons(i4,j4),inlats(i4,j4)
         ! Loop through output grid and calculate bilinear values
         ! loop through time also because input data has different time values.
-!        PRINT*,"STEP 3"
+        PRINT*,"STEP 3"
+        print*,"shape of ocs flux",shape(ocs_flux)
         do i = 1, OUT_NCOLS
          do j = 1, OUT_NROWS
-!          do t = 1, NSTEPS
-           t = 1
            lat = latgrid(i,j)
            lon = longrid(i,j)
+          call find_nearest4(lat,lon,inlats,inlons,nrows,ncols,&
+                i1,i2,i3,i4,j1,j2,j3,j4)
+          do t = 1, NSTEPS
 !           PRINT*, "STEP 4"
-           call find_nearest4(lat,lon,inlats,inlons,nrows,ncols,&
-                 i1,i2,i3,i4,j1,j2,j3,j4)
-           ! Get longitudes for interpolation      
+           ! Get longitudes for interpolation 
            x1 = inlons(i1,j1)
            x2 = inlons(i2,j2)
            x3 = inlons(i3,j3)
@@ -58,25 +84,114 @@
            z2 = in_cos_flux(i2,j2,t)
            z3 = in_cos_flux(i3,j3,t)
            z4 = in_cos_flux(i4,j4,t)
-!           if (z1.ne.0.0) then
-!            print*,"testing interp input", x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4
-!           endif
+         !  if (z1.ne.0.0) then
+         !   print*,"testing interp input", x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4
+         !  endif
            ! Perform interpolation to calculate value of OCS_FLUX as 'z'
-!           PRINT*,"STEP 5"
            call bilinear_interp(x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4,&
              lon,lat,z)
-           if(z.ne.0.0)then
-            print*,"non-zero-z",z
-           endif
            OCS_FLUX(i,j,t)=z
-!          enddo
+          enddo
          enddo
         enddo
 !        print*,"first layer",OCS_FLUX
+        print*,"max",maxval(OCS_FLUX),",min",minval(ocs_flux)
+
+      LOGDEV = INIT3()
+
+
+
+
+      PRINT*,'STEP4'
+      ! Write to ioapi
+      nvars3d=1                 ! number of variables
+      ftype3d=GRDDED3           ! file is in grided, Global dobson file header
+      gdtyp3d= 2!Lambert                !
+!      p_alp3d=90.              !unused in lat-lon
+!      p_bet3d=90.              !unused in lat-lon
+      xcent3d=36.94589                !unused in lat-lon
+      ycent3d=-119.6242                !unused in lat-lon
+      xorig3d=-122.0788
+      yorig3d=30.65546
+      xcell3d=4000.
+      ycell3d=4000.
+      ncols3d=out_ncols
+      nrows3d=out_nrows
+      nlays3d=1                ! documentation is vague on this maybe vertical levs
+      vgtyp3d=VGSGPN3           !  non-hydrostatic sigma-p vertical coordinate
+      vgtop3d=1.                ! domain top in meter
+      nthik3d=1
+      vglvs3d(1)=1.             ! levels in meter
+      units3d(1)='umol/m2/sec'
+      vname3d(1) = 'OCS_FLUX'
+      vdesc3d(1) = 'OCS FLUXES REGRIDDED FROM SIB'
+      vtype3d(1)=m3real
+
+      sdate3d = 2010121
+      stime3d = 000000
+      tstep3d = 010000
+
+       
+      print*, 'Attempting to open file...'
+      if(.not.OPEN3('outname',FSRDWR3,'gen_bdv')) then ! output file does not exit
+         print*, 'File does not exist, attempting file creation...'
+         if(.not.OPEN3('outname',FSCREA3,'gen_bdv')) then ! FSCREA3 FSUNKN3
+            print*, 'Error opening output file'
+            stop
+         endif
+      else
+         print*,'Reading from previously created file!'
+         if (.not. DESC3('outname') ) then ! if exit, get information
+            print*, 'Error getting info from OUTPUT nc'
+            stop
+         endif
+      endif
+
+! DATA WRITE SECTION
+      ! Setup time and iteration information.
+      day = 121
+      hour = 0
+      jdate =sdate3d
+      jtime = stime3d
+      print*,jdate,jtime
+      ! Attempt an I/O api data write. For whatever reason the convention is to
+      !     loop through time and write one 2d grid at a time.
+      do t=1,nsteps
+!       do i = 1,out_ncols
+!        do j = 1, out_nrows
+!          write_buffer(i,j) = OCS_FLUX(i,j,t)
+!        enddo
+!       enddo
+       !print*,"sum",sum(write_buffer),sum(OCS_FLUX),i,j,t
+        print*,"shape of ocs flux",shape(ocs_flux(:,:,1))
+       if(.not.write3('outname',vname3d(1),jdate,jtime,OCS_FLUX(:,:,t))) then
+          print*,'writing error'
+          stop
+       endif
+       print*, jdate,jtime
+       call nexttime(jdate,jtime,tstep3d, day, hour)
+      enddo
+
+
+      print*, 'SHUT3()=',SHUT3()
 
       end program bilinear_interpolation
 
-      
+      subroutine nexttime(jdate,jtime,tstep3d,day,hour)
+       implicit none
+       integer::jtime,jdate,tstep3d,day,hour
+       hour = hour+1
+       if (hour.eq.24)then
+        day = day+1
+        hour = 0
+       endif
+       jtime = hour*10000  
+       jdate = 2010*1000+day
+       print*, "new jtime", jtime
+       print*, "new jdate", jdate
+       return
+      end subroutine nexttime
+
       ! computes linear interpolation
       subroutine linear_interp(x0,x1,y0,y1,x,y)
        implicit none
@@ -123,12 +238,17 @@
          ztemp,&! final value
          z
 !       print*, "enter bilinear_interp()"
+!       if ((z0.ne.0.0).and.(z1.ne.0.0).and.(z2.ne.0.0).and.(z3.ne.0.0))then
+!        print*,"input: ",z0,z1,z2,z3
+!        print*,"answer: ",z
+!       endif
        z = 0.0
        yave1 = (y0+y2)/2.0
        yave2 = (y1+y3)/2.0
        call linear_interp(x0,x1,z0,z1,x,temp_z1)
        call linear_interp(x2,x3,z2,z3,x,temp_z2)
        call linear_interp(yave1,yave2,temp_z1,temp_z2,y,ztemp)
+       
        z = ztemp
 !       print*,"exit bilinear_interp()"
        return
@@ -203,19 +323,19 @@
        call check(nf90_get_var(ncid,varid,buffer))
        call check(nf90_close(ncid))
        print*,"File closed."
- 
        ! load longitude and latitude arrays
-       templat = -90.
-       templon = -180.
-       do i = nrows, 1,-1
-        do j = 1, ncols
-         inlats(j,i) = templat
-         inlons(j,i) = templon
-         templon=templon+xcell
-        enddo
-        templat=templat+ycell
-        templon = -180.
-       enddo
+!       templat = -90.
+!       templon = -180.
+!       print*, "cells xy",xcell,ycell
+!       do i = 1, ncols
+!        do j = 1 ,nrows
+!         inlats(i,j) = templat
+!         inlons(i,j) = templon
+!         templat=templat+ycell
+!        enddo
+!        templon=templon+xcell
+!        templat = -90.
+!       enddo
        
 !       do i=1,ncols
 !        print*,inlats(i,nrows)
@@ -225,7 +345,7 @@
        do i=1,ncols
         do j=1,nrows
          do t = 1,nsteps
-          VAR_DATA(j,i,t) = 0.0
+          VAR_DATA(i,j,t) = 0.0
          enddo
         enddo
        enddo
@@ -237,8 +357,24 @@
          VAR_DATA(lonindex(i),latindex(i),t) = BUFFER(i,t)
         enddo
        enddo
-       
-       
+       do i = 1,ncols
+        do j = 1, nrows
+         inlats(i,j) = -99999999999. 
+         inlons(i,j) = -99999999999.
+        enddo
+       enddo 
+       do i=1,181
+         inlats(lonindex(i),latindex(i)) = latitude(i)
+       enddo
+       do i = 1,288
+         inlons(lonindex(i),latindex(i)) = longitude(i)
+       enddo
+       print*,minval(lonindex),maxval(lonindex)
+       print*,minval(latindex),maxval(latindex) 
+       print*,"inlat range",minval(inlats),maxval(inlats)
+       print*,"inlon range",minval(inlons),maxval(inlons)
+       print*,"latrange",minval(latitude),maxval(latitude)
+       print*,"lonrange",minval(longitude),maxval(longitude)
        return
       end subroutine get_sib_variable   
 
@@ -277,11 +413,15 @@
             wrfout_d03_2010-05-25_00_R23_fpcut.nc",NF90_NOWRITE,ncid))
        print*,"Load variable: ","XLON"
        call check(nf90_inq_varid(ncid,"XLONG",lonid))
+       print*,"Load variable: ","XLAT"
        call check(nf90_inq_varid(ncid,"XLAT",latid))
-       call check(nf90_get_var(ncid,lonid,latgrid))
-       call check(nf90_get_var(ncid,latid,longrid))
- 
-
+       call check(nf90_get_var(ncid,latid,latgrid))
+       call check(nf90_get_var(ncid,lonid,longrid))
+       print*,"cat",latgrid(1,1)
+       print*,"cat",latgrid(2,2)
+       print*,"dog",longrid(1,1)
+       print*,"dog",longrid(2,2)
+       print*,"variables loaded successfully"
        call check(nf90_close(ncid))
       end subroutine make_out_grids
 
@@ -331,6 +471,7 @@
 !       print*,"enter find_nearest4()"
        ld = 9999999.0
        do i = 1, ncols
+!        if (abs(inlons(i,1)-lon).lt.10.0)then
         do j = 1, nrows
           call haversine(lat,lon,inlats(i,j),inlons(i,j),d)
           if (d.lt.ld) then
@@ -338,7 +479,8 @@
            tempi = i
            tempj = j
           endif
-        enddo
+         enddo
+!        endif
        enddo
        !note that the lats from inlat are flipped vertically so j values are intuitively backwards.
        if ((lat<inlats(tempi,tempj)).and.(lon<inlons(tempi,tempj))) then
@@ -377,6 +519,17 @@
           j2 = tempj - 1 
           j3 = tempj 
           j4 = tempj 
+       else
+          i1 = tempi - 1
+          i2 = tempi
+          i3 = tempi - 1
+          i4 = tempi
+          j1 = tempj
+          j2 = tempj
+          j3 = tempj - 1
+          j4 = tempj - 1
+          
        endif
+                
       return
       end subroutine find_nearest4
